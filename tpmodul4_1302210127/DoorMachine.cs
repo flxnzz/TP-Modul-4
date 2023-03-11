b6f5284435faf78d281edb98043b8a4a80c1aede
flxnzz <itszyro.47@gmail.com>
2023-03-11 13:46:37 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_1302210127/DoorMachine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class DoorMachine
+{
+    public enum doorState { Terkunci, Terbuka };
+    public enum Trigger { KunciPintu, BukaPintu };
+    public class Pintu
+    {
+        public doorState currentState = doorState.Terkunci;
+        public class Transition
+        {
+            public doorState stateAwal;
+            public doorState stateAkhir;
+            public Trigger trigger;
+            public Transition(doorState stateAwal, doorState stateAkhir, Trigger trigger)
+            {
+                this.stateAwal = stateAwal;
+                this.stateAkhir = stateAkhir;
+                this.trigger = trigger;
+            }
+        }
+
+        Transition[] transisi = { new Transition(doorState.Terkunci, doorState.Terkunci, Trigger.KunciPintu),
+            new Transition(doorState.Terkunci, doorState.Terbuka, Trigger.BukaPintu),
+            new Transition(doorState.Terbuka, doorState.Terbuka, Trigger.BukaPintu),
+            new Transition(doorState.Terbuka, doorState.Terkunci, Trigger.KunciPintu)
+        };
+
+        public doorState nextState(doorState stateAwal, Trigger trigger)
+        {
+            doorState finalState = stateAwal;
+
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition temp = transisi[i];
+                if (stateAwal == temp.stateAwal && trigger == temp.trigger)
+                {
+                    finalState = temp.stateAkhir;
+                }
+            }
+            return finalState;
+        }
+        public void activateTrigger(Trigger trigger)
+        {
+            currentState = nextState(currentState, trigger);
+            if (currentState == doorState.Terkunci)
+            {
+                Console.WriteLine("Pintu Terkunci");
+            }
+            else if (currentState == doorState.Terbuka)
+            {
+                Console.WriteLine("Pintu Terbuka");
+            }
+        }
+    }
+}
--- tpmodul4_1302210127/KodePos.cs
+++ tpmodul4_1302210127/KodePos.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿using static DoorMachine;
 public class KodePos
 {
     public enum daerah
@@ -7,11 +6,23 @@ public class KodePos
         Batununggal, Kujangsari, Mengger, Wates, Cijaura,
         Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
     }
-
     public static int getKodePos(daerah input)
     {
         int[] postalCode = { 40266, 40287, 40267, 40256, 40287,
         40286, 40286, 40286, 40272, 40274, 40273};
         return postalCode[(int)input];
+    }
+    public static void Main(string[] args)
+    {
+        // Soal 1 Kode Pos
+        daerah inputDaerah = daerah.Margasari;
+        int result = getKodePos(inputDaerah);
+        Console.WriteLine("Soal 1\nNama Daerah:\t" + inputDaerah +
+            "\nKode Pos:\t" + result + "\n");
+
+        // Soal 2 Pintu Buka/Tutup
+        Pintu door = new Pintu();
+        Console.WriteLine("Soal 2");
+        door.activateTrigger(Trigger.KunciPintu);
     }
 }
